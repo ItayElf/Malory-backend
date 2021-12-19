d2345a01c1c90c1238ba43e7537b4bc9090348df
ItayElf <thh123321@gmail.com>
2021-12-19 12:31:02 +0200

    handle auth function

--- endpoints/actions.py
+++ endpoints/actions.py
@@ -1,8 +1,24 @@
 import json
+from typing import Tuple
+
+import flask
 from flask import request
 from main import app
 from malory.orm.active_unit_orm import add_active_unit, active_unit_owner, delete_active_unit
 from malory.orm.user_orm import register_user, verify_user, get_user_idx
+
+
+def handle_auth(req: flask.Request) -> Tuple[str, int]:
+    """Tests if the username and password given are correct"""
+    if "username" not in request.form or "password" not in request.form:
+        return "Missing username or password in form", 400
+    username = request.form.get("username")
+    password = request.form.get("password")
+    try:
+        verify_user(username, password)
+    except ValueError:
+        return "Invalid user credentials", 401
+    return "", 200
 
 
 @app.route("/action/register", methods=["POST"])
@@ -19,12 +35,10 @@ def register_action():
 
 @app.route("/action/add_unit", methods=["POST"])
 def add_unit_action():
-    if "username" not in request.form or "password" not in request.form:
-        return "Missing username or password in form", 400
+    tup = handle_auth(request)
+    if tup[0]:
+        return tup
     username = request.form.get("username")
-    password = request.form.get("password")
-    if not verify_user(username, password):
-        return "Not a valid user credentials", 401
     if "unit_name" not in request.form:
         return "Missing unit_name in form", 400
     unit_name = request.form.get("unit_name")
@@ -38,19 +52,13 @@ def add_unit_action():
 
 @app.route("/action/remove_unit", methods=["DELETE"])
 def remove_unit_action():
-    if "username" not in request.form or "password" not in request.form:
-        return "Missing username or password in form", 400
+    tup = handle_auth(request)
+    if tup[0]:
+        return tup
     username = request.form.get("username")
-    password = request.form.get("password")
-    if not verify_user(username, password):
-        return "Not a valid user credentials", 401
     if "unit_idx" not in request.form or not request.form.get("unit_idx").isdigit():
         return "Missing unit_idx in form or it is not a valid one", 400
     unit_idx = int(request.form.get("unit_idx"))
     if get_user_idx(username) != active_unit_owner(unit_idx):
         return "Cannot remove unit of another player", 401
     return json.dumps(delete_active_unit(unit_idx))
-
-
-
-
